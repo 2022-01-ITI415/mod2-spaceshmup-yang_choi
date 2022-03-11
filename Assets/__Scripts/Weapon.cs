126db6e00a4666231c167b4dae6ca6f73f3f0bce
Eric Yang <ecy14@scarletmail.rutgers.edu>
2022-03-10 19:56:19 -0500

    Changed nothing

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -137,6 +137,7 @@ public class Weapon : MonoBehaviour {
                 p = MakeProjectile(); // Make middle Projectile
                 p.rigid.velocity = vel;
 
+//
                 p = MakeProjectile(); // Make right Projectile
                 p.transform.rotation = Quaternion.AngleAxis(10, Vector3.back);
                 p.rigid.velocity = p.transform.rotation * vel;
